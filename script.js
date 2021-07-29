8d16defce0b5788ca7526e042f884e6b89c355fa
Erick Borel <erickborel@hotmail.com>
2021-07-28 21:02:38 -0300

    Add photo of pokemons

---
+++ script.js
@@ -0,0 +1,30 @@
+var cartaBellsprout = {
+    Nome: Bellsprout,
+    imagem: `<img src = "img/Bellsprout.PNG"/>`,
+
+}
+
+var cartaBulbasaur = {
+    nome: Bulbasaur,
+    imagem: `<img src = "img/Bulbasaur.PNG"/>`
+}
+
+var cartaCaterpie = {
+    nome: Caterpie,
+    imagem: `<img src = "img/Caterpie.PNG"/>`
+}
+
+var cartaCharmander = {
+    nome: Caterpie,
+    imagem: `<img src = "img/Charmander.PNG"/>`
+}
+
+var cartaClefairy = {
+    nome: Caterpie,
+    imagem: `<img src = "img/Clefairy.PNG"/>`
+}
+
+var cartaDiglett = {
+    nome: Caterpie,
+    imagem: `<img src = "img/Diglett.PNG"/>`
+}
