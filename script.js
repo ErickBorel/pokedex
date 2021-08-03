ab3114c20750e45350aa921f8f7bbf777af3d373
Erick Borel <erickborel@hotmail.com>
2021-08-03 13:14:03 -0300

    creating (pokemon) objects

--- script.js
+++ script.js
@@ -28,3 +28,69 @@ var cartaDiglett = {
     nome: Caterpie,
     imagem: `<img src = "img/Diglett.PNG"/>`
 }
+
+var cartaEkans = {
+    nome: Ekans,
+    imagem: `<img src = "img/Ekans.PNG"/>`
+}
+
+var cartaKoffing = {
+    nome: Koffing,
+    imagem: `<img src = "img/Koffing.PNG"/>`
+}
+
+var cartaMeowth = {
+    nome: Meowth,
+    imagem: `<img src = "img/Meowth.PNG"/>`
+}
+
+var cartaOddish = {
+    nome: Oddish,
+    imagem: `<img src = "img/Oddish.PNG"/>`
+}
+
+var cartaPidgey = {
+    nome: Pidgey,
+    imagem: `<img src = "img/Pidgey.PNG"/>`
+}
+
+var cartaPikachu = {
+    nome: Pikachu,
+    imagem: `<img src = "img/Pikachu.PNG"/>`
+}
+
+var cartaPsyduck = {
+    nome: Psyduck,
+    imagem: `<img src = "img/Psyduck.PNG"/>`
+}
+
+var cartaRattata = {
+    nome: Rattata,
+    imagem: `<img src = "img/Rattata.PNG"/>`
+}
+
+var cartaSpearow = {
+    nome: Spearow,
+    imagem: `<img src = "img/Spearow.PNG"/>`
+}
+
+var cartaSquirtle = {
+    nome: Squirtle,
+    imagem: `<img src = "img/Squirtle.PNG"/>`
+}
+
+var cartaWeedle = {
+    nome: Weedle,
+    imagem: `<img src = "img/Weedle.PNG"/>`
+}
+
+let pokemons = [cartaBellsprout, cartaBulbasaur, cartaCaterpie, cartaCharmander, cartaClefairy, cartaDiglett, cartaEkans, cartaKoffing, cartaMeowth, cartaOddish, cartaPidgey, cartaPikachu, cartaPsyduck, cartaRattata, cartaSpearow, cartaSquirtle, cartaWeedle]
+function iniciar() {
+
+    
+    var img = document.getElementById('quest');
+    img.innerHTML = `<img src ="img/Koffing.PNG"/>`
+    
+}
+
+console.log(pokemons)
